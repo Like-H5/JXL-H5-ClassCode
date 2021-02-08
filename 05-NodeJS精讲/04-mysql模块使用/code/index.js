116d0edbfc4f5d038c8e41a9df714a383718c535
wangshunzi <501562071@qq.com>
2021-02-08 09:48:05 +0800

    章节代码调整

--- 05-NodeJS精讲/04-mysql模块使用/code/index.js
+++
@@ -1,9 +0,0 @@
-const {execSQL} = require("./tool/sz_mysql")
-
-execSQL("select * from t_nav;").then(result=>{
-    console.log(result)
-})
-
-execSQL("select * from t_news;").then(result=>{
-    console.log(result)
-})
--- 05-NodeJS精讲/05-数据重构案例/02-实战案例-轮播图/client/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-let app = express();
-
-app.use(express.static("public"))
-
-app.listen(3000, () => {
-    console.log("前端服务器启动成功")
-})
